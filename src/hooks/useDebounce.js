5c2af9fdbda28bbd8bdb2fd0b67eb06cfa15265f
Kartik Gupta <k.a.r.t.i.k.2007.17@gmail.com>
2026-04-01 12:31:23 +0530

    debiunce update

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-const useDebounce = (value, delay = 300) => {
+const useDebounce = (value, delay = 450) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const timerId = setTimeout(() => {
